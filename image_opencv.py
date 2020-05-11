c43b6291224a70b098277ea8710ee5c7499c5e7a
Mohammedsalah98 <Mohammedghrably98@gmail.com>
2020-05-12 00:19:09 +0200

    just changed the file name from opencv to image_opencv

---
+++ image_opencv.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+import matplotlib as plt
+
+# careful with the identions
+
+"""
+# reading an image can be done in opencv with many ways:
+# IMREAD_GRAYSCALE , we can basicly just replace it with 0
+# IMREAD_COLOR , or just replace it with 1
+ """
+
+# reading a randam image from my labtop using opencv
+image = cv2.imread('programming.jpg',cv2.IMREAD_GRAYSCALE)
+
+# showing the image i just read
+cv2.imshow('Image' , image)
+
+# litarly waiting for any button to be presed
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
