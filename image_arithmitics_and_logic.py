ea2e5fad2501ecb719cd73924d7b55afb26f5802
Mohammedsalah98 <Mohammedghrably98@gmail.com>
2020-05-12 23:00:17 +0200

    i tried the builtin add method

--- image_arithmitics_and_logic.py
+++ image_arithmitics_and_logic.py
@@ -7,5 +7,9 @@ image2 = cv2.imread('mainsvmimage.png')
 addition = image1 + image2
 
 cv2.imshow('addition', addition)
+
+# let's try the addition method which is built in cv
+add = cv2.add(image1,image2)
+cv2.imshow('built_in_add', add)
 cv2.waitKey(0)
 cv2.destroyAllWindows(0)
