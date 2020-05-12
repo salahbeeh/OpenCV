beb683d0fbab5251b0c8113b0f96c699617b5eda
Mohammedsalah98 <Mohammedghrably98@gmail.com>
2020-05-12 23:10:15 +0200

    apply the weighted method to impose images

--- image_arithmitics_and_logic.py
+++ image_arithmitics_and_logic.py
@@ -11,5 +11,12 @@ cv2.imshow('addition', addition)
 # let's try the addition method which is built in cv
 add = cv2.add(image1,image2)
 cv2.imshow('built_in_add', add)
+
+# i can try imposing the images in another way
+# waghited images
+# cv2.addWeighted( frist_image ,its weight , second_image, its weight, gama value)
+# don't fuck with the gama value and leave it alone as 0
+weighted = cv2.addWeighted(image1,0.6, image2 , 0.4,0)
+cv2.imshow('weighted', weighted)
 cv2.waitKey(0)
 cv2.destroyAllWindows(0)
