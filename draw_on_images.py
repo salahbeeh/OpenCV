4cc9a0e0073344207f37e57ceceb0ca542a041ab
Mohammedsalah98 <Mohammedghrably98@gmail.com>
2020-05-12 04:48:11 +0200

    added a new script for drawing on images
    added a code line that allow cv to draw a line on the image

---
+++ draw_on_images.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+# just reading the image
+image = cv2.imread('programming.jpg',cv2.IMREAD_COLOR)
+
+# drawing a line
+# cv2.line(image,srart point,end point,line_color, line width)
+cv2.line(image,(5,5),(60,60),(255,255,255),5)
+
+
+
+
+
+cv2.imshow('image',image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
