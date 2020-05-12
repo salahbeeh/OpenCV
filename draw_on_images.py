84378f038aa08f55d23605d59416166db70a2174
Mohammedsalah98 <Mohammedghrably98@gmail.com>
2020-05-12 04:57:21 +0200

    draw a rectangle

--- draw_on_images.py
+++ draw_on_images.py
@@ -8,7 +8,9 @@ image = cv2.imread('programming.jpg',cv2.IMREAD_COLOR)
 # cv2.line(image,srart point,end point,line_color, line width)
 cv2.line(image,(5,5),(60,60),(255,255,255),5)
 
-
+# drawing a rectangle
+# cv2.rectangle(image,highest lift point ,lowest right point,rectangle_color, rectangle width)
+cv2.rectangle(image,(100,100) ,(350,350),(0,255,0), 5)
 
 
 
