2be3cd165928d9d11b883adc2f0c1886976c5c5e
Mohammedsalah98 <Mohammedghrably98@gmail.com>
2020-05-13 21:33:32 +0200

    try bluring methods
    after doing the color filtering there was still some noise which I found it annoying so I tried 3 diffrent ways of bluring to get rid of the noise and they are the averriging and the gaussian bluring and the median bluring and found the median is the best as it gives the best results but still not enough!.

--- filtering_for_color_range.py
+++ filtering_for_color_range.py
@@ -9,13 +9,28 @@ while True:
     hsv = cv.cvtColor(frame ,cv.COLOR_BGR2HSV)
 
     # hsv hue sat Value
-    lower  = np.array([150,0,0])
-    higher = np.array([220,255,255])
+    lower  = np.array([130,0,0])
+    higher = np.array([200,255,255])
 
     mask   = cv.inRange(hsv,lower,higher)
     result = cv.bitwise_and(frame,frame,mask =mask)
 
-    cv.imshow('mask',mask)
+
+    # now if we wanna get rid of the noise in the background
+    # let's try smoothing with averaging
+    kernel   = np.ones((15,15), np.float32)/225
+    smoothed = cv.filter2D(result, -1, kernel)
+    cv.imshow('smoothed', smoothed)
+
+    # let's try gaussian bluring
+    blur = cv.GaussianBlur(result,(15,15),0)
+    cv.imshow('gaussian', blur)
+
+    # let's try median
+    median = cv.medianBlur(result,15)
+    cv.imshow('median', median)
+
+    #cv.imshow('mask',mask)
     cv.imshow('result', result)
 
     k = cv.waitKey(5) & 0xFF
