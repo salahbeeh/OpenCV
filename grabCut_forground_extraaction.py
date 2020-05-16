5805b14ddee4b8622f705797ab1ed79a0419f2a0
Mohammedsalah98 <Mohammedghrably98@gmail.com>
2020-05-16 04:27:34 +0200

    just changed the image directory

--- grabCut_forground_extraaction.py
+++ grabCut_forground_extraaction.py
@@ -2,13 +2,13 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-img = cv2.imread('me.jpg')
+img = cv2.imread('samples/me.jpg')
 mask = np.zeros(img.shape[:2],np.uint8)
 
 bgdModel = np.zeros((1,65),np.float64)
 fgdModel = np.zeros((1,65),np.float64)
 # rect = It is the coordinates of a rectangle which includes the foreground object in the format (x,y,w,h)
-rect = (450,50,450,900)
+rect = (700,200,200,300)
 
 cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
 mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
